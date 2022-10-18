0dc317f6f6dcb8dac0a17b3ed10553570688cfb3
31aditya13 <116092009+31aditya13@users.noreply.github.com>
2022-10-18 19:57:15 +0530

    Add files via upload

---
+++ project/index.js
@@ -0,0 +1,25 @@
+let myBtn=document.getElementById('btn');
+
+function openCompiler()
+{
+    window.open("form.html")
+    document.getElementById(compiler).style.display="block";
+}
+
+
+let gridEvent=document.getElementById('gridEvent');
+
+function gridOne()
+{
+    window.open("form2.html")
+    document.getElementById(compiler).style.display="block";
+}
+
+
+let gridEventTwo=document.getElementById('gridEventTwo');
+
+function gridTwo()
+{
+    window.open("form3.html")
+    document.getElementById(compiler).style.display="block";
+}
